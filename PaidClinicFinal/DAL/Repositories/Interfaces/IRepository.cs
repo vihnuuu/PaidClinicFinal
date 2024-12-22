9eec3226eb24590997a0ca4261f090269610e024
Viktoriia Khniukalo <vi.hnuuu@gmail.com>
2024-12-23 01:57:21 +0200

    Added Contracts of Repositories

---
+++ PaidClinicFinal/DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(); // отримати всі записи
+        Task<T> GetByIdAsync(int id); // отримати запис за айді
+        Task AddAsync(T entity); // додати новий запис
+        Task UpdateAsync(T entity); // оновити існуючий запис
+        Task DeleteAsync(int id); // видалити запис за айді
+    }
+}
