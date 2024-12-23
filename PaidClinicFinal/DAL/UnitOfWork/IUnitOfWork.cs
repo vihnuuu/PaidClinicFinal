4d709a410d2188f73233a2cd7f9b636eb4da4e01
Viktoriia Khniukalo <vi.hnuuu@gmail.com>
2024-12-23 02:20:55 +0200

    Implemented Unit of Work pattern with IUnitOfWork interface

---
+++ PaidClinicFinal/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPatientRepository Patients { get; }
+        IDoctorRepository Doctors { get; }
+        IVisitRepository Visits { get; }
+        ITreatmentRepository Treatments { get; }
+        void Save();
+    }
+}
